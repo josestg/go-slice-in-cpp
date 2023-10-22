56a9e85b2a9b01526276964cb8d8b1e5206a9803
Jose Alfredo Sitanggang <josealfredositanggang@gmail.com>
2023-10-22 21:26:21 +0700

    chore: update demo

---
+++ README.md
@@ -0,0 +1,21 @@
+# Exploring Go Slice Internal Implementation in C++
+
+See [blog post](https://www.josestg.com/blog/exploring-slice-internal-implementation-go) for details.
+
+## Build
+
+```bash
+cmake -B build -S . && cmake --build build
+```
+
+## Run Test
+
+```bash
+./build/go_slice_in_cpp_test
+```
+
+## Run Demo
+
+```bash
+./build/go_slice_in_cpp_demo
+```
--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include "slice.h"
 
-
 int main() {
   Slice s = make(0, 1);
+  std::cout << "slice created: " << s << std::endl;
+
+  int last_cap = s.cap;
   for (int i = 0; i < 10; i++) {
+    std::cout << "append value: " << i << std::endl;
     s = append(s, i);
+    if (s.cap != last_cap) {
+      std::cout << "slice capacity changed: " << s << std::endl;
+      last_cap = s.cap;
+    }
   }
 
-  std::cout << s << std::endl;
+  std::cout << "slice: " << s << std::endl;
+
+  std::cout << "updating slice[0] to 100" << std::endl;
+  set_value(s, 0, 100);
+
+  std::cout << "slice after update: " << s << std::endl;
   free_slice(s);
   return 0;
 }
